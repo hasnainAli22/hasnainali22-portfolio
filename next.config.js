d83fbada0f323994eba53b04bb869436c616f587
hasnainAli22 <our.hasnain22@gmail.com>
2024-07-15 02:55:26 +0500

    Revert "edded log statement config"
    This reverts commit f3b394b2860b953353361b765e3af83f058ef464.

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const path = require("path");
+// const { withSentryConfig } = require("@sentry/nextjs");
+const isDev = true ? process.env.NODE_ENV === 'development' : false;
+const nextConfig = {
+  reactStrictMode: true,
+  output: isDev ? "standalone" : "export",
+  basePath: isDev ? "" : "/hasnainali22-portfolio",
+  assetPrefix: isDev ? "" : '/hasnainali22-portfolio/',
+  // basePath: '/hasnainali22-portfolio', // Replace with your GitHub repository name
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "**.githubusercontent.com" },
+      { protocol: "https", hostname: "**.github.com" },
+    ],
+  },
+  publicRuntimeConfig: {
+    basePath: isDev ? "" : "/hasnainali22-portfolio",
+  },
+};
+module.exports = nextConfig;
+// module.exports = withSentryConfig(nextConfig, {
+//   org: "nixlab-technologies",
+//   project: "portfolio-nextjs",
+//   authToken: process.env.SENTRY_AUTH_TOKEN,
+//   silent: false,
+// });
