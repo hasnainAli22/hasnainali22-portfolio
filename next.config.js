f50327273986b3221df073e5b3f8cf4c46a7816f
hasnainAli22 <our.hasnain22@gmail.com>
2024-07-14 21:41:25 +0500

    edited the next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 // const { withSentryConfig } = require("@sentry/nextjs");
-
+const isDev = true ? process.env.NODE_ENV === 'development' : false;
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  basePath: '/hasnainali22-portfolio', // Replace with your GitHub repository name
-  assetPrefix: '/hasnainali22-portfolio/',
-  // output: "export",
+  output: isDev ? "standalone" : "export",
+  basePath: isDev ? "" : "/hasnainali22-portfolio",
+  assetPrefix: isDev ? "" : '/hasnainali22-portfolio/',
+  // basePath: '/hasnainali22-portfolio', // Replace with your GitHub repository name
   swcMinify: true,
   images: {
     remotePatterns: [
